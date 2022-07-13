c2d533f6bcaaf8fed0ba62b23d588040e0889d14
yueyuen <431814753@qq.com>
2022-07-13 22:30:41 +0800

    initagain

---
+++ src/test/java/com/personalblog/PersonalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
